c741030e6ae5b3d881dfa967849eb734e658e243
Jonathan Perez <jonathan.phz@gmail.com>
2016-11-21 12:20:23 -0300

    Add browsersync to gulpfile

--- los-datos-y-vos/gulpfile.js
+++ los-datos-y-vos/gulpfile.js
@@ -3,6 +3,17 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var ngAnnotate = require('gulp-ng-annotate');
 var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+
+gulp.task('browserSync', function() {
+	browserSync.init({
+		server: {
+		  baseDir: ''
+		},
+	})
+})
 
 gulp.task('js', function () {
 	gulp.src(['src/**/module.js', 'src/**/*.js'])
@@ -15,13 +26,18 @@ gulp.task('js', function () {
 });
 
 gulp.task('sass', function(){
-  return gulp.src('/scss/style.scss')
+  return gulp.src('scss/**/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest('/css'))
+    .pipe(gulp.dest('css'))
+	.pipe(browserSync.reload({
+    	stream: true
+    }))
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', ['browserSync', 'sass'], function () {
 	gulp.watch('src/**/*.js', function() {
 		gulp.src(['src/**/module.js', 'src/**/*.js']).pipe(concat('app.js')).pipe(gulp.dest('.'));
 	});
+	gulp.watch('scss/**/*.scss', ['sass']);
+	gulp.watch('/*.html', browserSync.reload);
 });
