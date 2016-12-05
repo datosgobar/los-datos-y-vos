e1fdae61143037ede97ef1b0baabd982abb8106b
Max Di Franco <jonathan.difranco@gmail.com>
2016-12-05 11:33:19 -0300

    Svg location Map

---
+++ los-datos-y-vos/src/app/directives/locationMap.directive.js
@@ -0,0 +1,30 @@
+angular.module('app').directive("svgMap", function() {
+  return {
+    restrict: "E",
+    replace: true,
+    template: "<object type='image/svg+xml' id='locationMap'></object>",
+    link: function(scope, element, attrs) {
+      var provinceChanged = function(newValue, oldValue) {
+        var locationMap = angular.element(document.querySelector('#locationMap'));
+        if(newValue.id) {
+          locationMap[0].data = "/img/maps/" + newValue.id + ".svg";
+        }
+      };
+      var departmentChanged = function(newValue, oldValue) {
+        var locationMap = angular.element(document.querySelector('#locationMap'));
+        if(newValue.id) {
+          var oldSelectedDepartment = angular.element(element[0].getSVGDocument().getElementsByClassName("selected"));
+          oldSelectedDepartment.removeClass("selected");
+          if(oldSelectedDepartment.length > 0) {
+            oldSelectedDepartment[0].children[0].style.fill="white";
+          }
+          var selectedDepartment = angular.element(element[0].getSVGDocument().getElementById(("00" + newValue.id).slice(-5)));
+          selectedDepartment.addClass("selected");
+          selectedDepartment.children('path')[0].style.fill="black";
+        }
+      };
+      scope.$watch(attrs.province, provinceChanged);
+      scope.$watch(attrs.department, departmentChanged);
+    }
+  }
+});
