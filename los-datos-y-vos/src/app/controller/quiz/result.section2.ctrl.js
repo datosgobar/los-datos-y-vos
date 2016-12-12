816183164fe109b9ca3964704b4451bd3f7744ea
Agustín Benassi <agusbenassi@gmail.com>
2016-12-12 16:24:06 -0300

    Modificamos otros textos.

--- los-datos-y-vos/src/app/controller/quiz/result.section2.ctrl.js
+++ los-datos-y-vos/src/app/controller/quiz/result.section2.ctrl.js
@@ -1,12 +1,12 @@
 angular.module('app').controller('ResultSection2Ctrl', function($scope, $state, StudentDataSvc, QuizSvc, LocationIndicatorSvc) {
-    
+
     var activate = function() {
         $scope.studentData = StudentDataSvc.getStudentData();
         $scope.sectionData = QuizSvc.getSectionData($state.current.data.stepNumber);
-        
+
         $scope.keys = [];
         $scope.keys['YOUR_DEPARTMENT'] = $scope.studentData.province.id == 2 ? 'comuna' : 'departamento';
-        $scope.keys['YOUR_PROVINCE'] = ($scope.studentData.province.id == 2 ? "La " : "La Provincia de ") + $scope.studentData.province.name;
+        $scope.keys['YOUR_PROVINCE'] = ($scope.studentData.province.id == 2 ? "" : "Provincia de ") + $scope.studentData.province.name;
 
         $scope.calculateResults();
     };
@@ -19,25 +19,25 @@ angular.module('app').controller('ResultSection2Ctrl', function($scope, $state, 
 
     $scope.calculateResults = function() {
         $scope.results = {};
-        
+
         angular.forEach($scope.sectionData.pages, function(sectionPage) {
             if(sectionPage.id == "avgPersonsPerHouse") {
                 var questionResults = {
-                    questionText: "Personas que duermen por cuarto en promedio:",
+                    questionText: "Personas que duermen por habitación en promedio:",
                     options: []
                 };
-                questionResults.options.push({ 
-                    optionText: "YOUR_HOUSE",  
+                questionResults.options.push({
+                    optionText: "YOUR_HOUSE",
                     yourAnswer: $scope.studentData[$scope.sectionData.id]["personsPerHouse"] /
                         $scope.studentData[$scope.sectionData.id]["roomsPerHouse"]
                 });
-                questionResults.options.push({ 
-                    optionText: "YOUR_DEPARTMENT",  
+                questionResults.options.push({
+                    optionText: "YOUR_DEPARTMENT",
                     yourAnswer: $scope.studentData[$scope.sectionData.id]["avgPersonsPerHouse"]['department'],
                     censusResult: $scope.studentData.department["avgPersonsPerHouse"]
                 });
-                questionResults.options.push({ 
-                    optionText: "YOUR_PROVINCE",  
+                questionResults.options.push({
+                    optionText: "YOUR_PROVINCE",
                     yourAnswer: $scope.studentData[$scope.sectionData.id]["avgPersonsPerHouse"]['province'],
                     censusResult: $scope.studentData.province["avgPersonsPerHouse"]
                 });
@@ -51,8 +51,8 @@ angular.module('app').controller('ResultSection2Ctrl', function($scope, $state, 
                     if(question.id == "rentedHouse") {
                         var rentedHouseResults = {
                             questionText: "Vive en un lugar alquilado:",
-                            optionText: "Vivís en una vivienda alquilada?",  
-                            yourAnswer: parseInt($scope.studentData[$scope.sectionData.id]["rentedHouse"]) ? 'Si' : 'No'
+                            optionText: "Vivís en una vivienda alquilada?",
+                            yourAnswer: parseInt($scope.studentData[$scope.sectionData.id]["rentedHouse"]) ? 'Sí' : 'No'
                         };
                         avgRentingHouseResults.rentedHouse = rentedHouseResults
                     }
