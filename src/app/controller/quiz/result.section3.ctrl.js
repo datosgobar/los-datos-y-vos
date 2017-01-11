c2200af02b8beb1c8485c1b3beb71e1d706534fe
Max Di Franco <jonathan.difranco@gmail.com>
2017-01-11 12:41:10 -0300

    Added values for the tooltip depending on the variable selected.

--- src/app/controller/quiz/result.section3.ctrl.js
+++ src/app/controller/quiz/result.section3.ctrl.js
@@ -1,8 +1,12 @@
-angular.module('app').controller('ResultSection3Ctrl', function($scope, $state, StudentDataSvc) {
+angular.module('app').controller('ResultSection3Ctrl', function($scope, $state, StudentDataSvc, LocationIndicatorSvc) {
 
     var activate = function() {
         $scope.studentData = StudentDataSvc.getStudentData();
         $scope.comparisonType = 'youngProportion';
+
+        LocationIndicatorSvc.getNeighbourhoodList().then(function(data) {
+        	$scope.departmentList = data;
+        });
     };
 
     activate();
--- src/app/directives/comparisonMap.directive.js
+++ src/app/directives/comparisonMap.directive.js
@@ -1,40 +1,53 @@
-angular.module('app').directive("comparisonMap", function(LocationIndicatorSvc) {
+angular.module('app').directive("comparisonMap", function($filter) {
   return {
     restrict: "E",
     replace: true,
+    scope: {
+      valueList: '<',
+      comparisonType: '='
+    },
     template: "<object type='image/svg+xml' id='comparisonMap' class='student-info__maps'></object>",
     link: function(scope, element, attrs) {
+      
+      var provinceChanged = function(newValue, oldValue) {
+        if(newValue) {
+          var object = document.getElementById("comparisonMap");
+          object.data = "img/maps/" + newValue + ".svg";
+          object.addEventListener('load', init);
+        }
+      };
+
+      var comparisonTypeChanged = function(newValue, oldValue) {
+          // TODO HIDE TOOLTIPS
+      };
+
+      var showDepartmentTooltip = function(event) {
+        var departmentId = parseInt(event.currentTarget.id);
+        var department = $filter('filter')(scope.valueList, {id: departmentId})[0];
+        var normalizedValue;
+        if(scope.comparisonType == 'avgPersonsPerHouse') {
+          normalizedValue = Math.round(department[scope.comparisonType]);
+        } else {
+          normalizedValue = Math.round(department[scope.comparisonType] * 100) + '%';
+        }
+        // Set this value to the tooltip
+        console.log(normalizedValue);
+      };
+
       var init = function() {
         var pathElements = angular.element(element[0].getSVGDocument().getElementsByTagName("g"));
-
         for(var i = 0; i < pathElements.length; i++) {
             element = pathElements[i];
             if (element.id != null && !isNaN(element.id) && element.id != attrs.province) {
-              element.addEventListener('click', function(event) {
-                console.log(parseInt(event.currentTarget.id));
-              });
+              element.classList.add("department")
+              element.addEventListener('click', showDepartmentTooltip);
             }
         }
-
-        var comparisonTypeChanged = function(newValue, oldValue) {
-          
-        };
         scope.$watch(attrs.comparisonType, comparisonTypeChanged);
-
       };
 
-      var provinceChanged = function(newValue, oldValue) {
-        if(newValue) {
-          element[0].data = "img/maps/" + newValue + ".svg";
-        }
-      };
       scope.$watch(attrs.province, provinceChanged);
 
-      if (element[0].getSVGDocument()) {
-        init();
-      } else {
-        element.on('load', init);
-      }
     }
   }
 });
