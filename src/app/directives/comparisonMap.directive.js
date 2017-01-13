9af701c73ecd1ede184f465a5294eb91162402f8
Jonathan Perez <jonathan.phz@gmail.com>
2017-01-13 15:25:28 -0300

    Add tooltip code to comparison map

--- src/app/directives/comparisonMap.directive.js
+++ src/app/directives/comparisonMap.directive.js
@@ -8,7 +8,7 @@ angular.module('app').directive("comparisonMap", function($filter) {
     },
     template: "<object type='image/svg+xml' id='comparisonMap' class='student-info__maps'></object>",
     link: function(scope, element, attrs) {
-      
+
       var provinceChanged = function(newValue, oldValue) {
         if(newValue) {
           var object = document.getElementById("comparisonMap");
@@ -25,6 +25,9 @@ angular.module('app').directive("comparisonMap", function($filter) {
         var departmentId = parseInt(event.currentTarget.id);
         var department = $filter('filter')(scope.valueList, {id: departmentId})[0];
         var normalizedValue;
+        var description = document.getElementById("descriptionDiv");
+        var mapWrapper = document.getElementById("comparisonMap");
+        description.className = "description active";
         if(scope.comparisonType == 'avgPersonsPerHouse') {
           normalizedValue = Math.round(department[scope.comparisonType]);
         } else {
@@ -32,6 +35,15 @@ angular.module('app').directive("comparisonMap", function($filter) {
         }
         // Set this value to the tooltip
         console.log(normalizedValue);
+        //Position Tooltip were mouse clicked
+          description.style.left = event.clientX + mapWrapper.offsetLeft + "px";
+          //the 100 is used because of the tooltip height
+          description.style.top =  event.clientY + mapWrapper.offsetTop - 100 + "px";
+
+          //Get data-info value
+          var info = this.getAttribute("id");
+          //Add information from data-info to HTML element
+          description.innerHTML = normalizedValue;
       };
 
       var init = function() {
