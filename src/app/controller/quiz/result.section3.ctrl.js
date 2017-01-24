2dddc90d6642ee35d2d76903b987ce7e5c441806
Max Di Franco <jonathan.difranco@gmail.com>
2017-01-24 20:08:19 -0300

    mapa interactivo cambio de provincia

--- src/app/controller/quiz/result.section3.ctrl.js
+++ src/app/controller/quiz/result.section3.ctrl.js
@@ -4,17 +4,54 @@ angular.module('app').controller('ResultSection3Ctrl', function($scope, $state, 
         $scope.studentData = StudentDataSvc.getStudentData();
         $scope.comparisonType = 'youngProportion';
         $scope.currentDepartment = null;
-        LocationIndicatorSvc.getNeighbourhoodList().then(function(data) {
-        	$scope.departmentList = $filter('orderBy')(data, 'avgPersonsPerHouse');
-            $scope.avgPersonsPerHouse = {
-                min: $scope.departmentList[0].avgPersonsPerHouse,
-                max: $scope.departmentList[$scope.departmentList.length - 1].avgPersonsPerHouse
-            };
-            console.log($scope.avgPersonsPerHouse);
+        $scope.currentValue = 100;
+        $scope.mapControl = {};
+
+        initPieChart();
+
+        LocationIndicatorSvc.getProvinceList().then(function(data) {
+            $scope.provinceList = data;
         });
-		$scope.currentValue = 100;
+        $scope.$watch("studentData.province", function(){
+            initMap();
+            updateDepartmentList();
+        });
+
+    };
+
+    var initMap = function() {
+        var comparisonMap = document.getElementById("comparisonMap");
+        comparisonMap.data = "img/maps/" + $scope.studentData.province.id + ".svg";
+        comparisonMap.onload = function() {
+            $scope.closeTooltip();
+            var pathElements = angular.element(comparisonMap.getSVGDocument().getElementsByTagName("g"));
+            var rootElement;
+            for(var i = 0; i < pathElements.length; i++) {
+                var pathElement = pathElements[i];
+                if (pathElement.id != null && !isNaN(pathElement.id) 
+                    && pathElement.id == $scope.studentData.province.id) {
+                  rootElement = pathElement;
+                  rootElement.setAttribute('class', $scope.comparisonType);
+                }
+            }
+            $scope.mapControl.map = rootElement;
+            for(var i = 0; i < pathElements.length; i++) {
+                var pathElement = pathElements[i];
+                if (pathElement.id != null && !isNaN(pathElement.id) 
+                    && pathElement.id != $scope.studentData.province.id) {
+                    pathElement.classList.add("department")
+                    pathElement.addEventListener('click', function(event) {
+                    removeActiveTooltips();
+                    showDepartmentTooltip(event);
+              });
+            }
+        }
+        };
+    };
+
+    var initPieChart = function() {
         $scope.pieChartOptions = {
-       		animate:{
+            animate:{
                 duration:0,
                 enabled:false
             },
@@ -32,14 +69,35 @@ angular.module('app').controller('ResultSection3Ctrl', function($scope, $state, 
             size: 70,
             trackColor: "#EEEEEE"
         };
+    };
 
-        $scope.mapControl = {};
+    var updateDepartmentList = function() {
+        var provinceId = $scope.studentData.province && $scope.studentData.province.id ? $scope.studentData.province.id : 2;
+        if(provinceId == 2) { // CABA
+            LocationIndicatorSvc.getNeighbourhoodList().then(function(data) {
+                $scope.departmentList = $filter('orderBy')(data, 'avgPersonsPerHouse');
+                $scope.avgPersonsPerHouse = {
+                    min: $scope.departmentList[0].avgPersonsPerHouse,
+                    max: $scope.departmentList[$scope.departmentList.length - 1].avgPersonsPerHouse
+                };
+            });
+        } else {
+            LocationIndicatorSvc.getDepartmentList().then(function(data) {
+                $scope.departmentList = $filter('orderBy')($filter('filter')(data, {provinceId: provinceId}, true),
+                    'avgPersonsPerHouse');
+                $scope.avgPersonsPerHouse = {
+                    min: $scope.departmentList[0].avgPersonsPerHouse,
+                    max: $scope.departmentList[$scope.departmentList.length - 1].avgPersonsPerHouse
+                };
+
+            });
+        }
     };
 
     $scope.closeTooltip = function(event) {
     	var description = document.getElementById("descriptionDiv");
         description.classList.remove("active");
-        $scope.mapControl.removeActiveTooltips();
+        removeActiveTooltips();
     };
 
     $scope.changeComparisonType = function(type) {
@@ -60,8 +118,7 @@ angular.module('app').controller('ResultSection3Ctrl', function($scope, $state, 
         }
   	};
 
-    $scope.showDepartmentTooltip = function(event) {
-
+    var showDepartmentTooltip = function(event) {
     	$scope.$apply(function(){
     		var departmentId = parseInt(event.currentTarget.id);
 	        var department = $filter('filter')($scope.departmentList, {id: departmentId})[0];
@@ -79,6 +136,15 @@ angular.module('app').controller('ResultSection3Ctrl', function($scope, $state, 
 	      	description.style.top =  event.clientY + mapWrapper.offsetTop - 118 + "px";
 	      	$scope.updateNormalizedValue();
     	});
+    };
+
+    var removeActiveTooltips = function() {
+        var map = document.getElementById("comparisonMap");
+        var pathElements = angular.element(map.getSVGDocument().getElementsByClassName("department active"));
+        for(var i = 0; i < pathElements.length; i++) {
+            var pathElement = pathElements[i];
+            pathElement.classList.remove("active");
+        }
     };
 
     activate();
